b0614314719fe53f5fd6a4319025c3b333966167
Lina Hsu <linahthsu@gmail.com>
2024-09-24 13:20:48 -0300

    feat: implements css for footer and header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,20 @@
 import whatsappIcon from '../assets/64px-WhatsApp_icon.png';
+import './Footer.css';
 
 function Footer() {
   const whatsappLink = 'https://wa.me/5511978149428';
 
   return (
     <footer>
-      <div>
+      <a className='whatsapp-icon' href={whatsappLink} target="_blank">
+        <img src={whatsappIcon} alt='Whatsapp Icon' />
+      </a>
+
+      <div className='footer-info'>
         <p>Rua Afonso Celso, 1679 - Vila Mariana</p>
         <p>Telefone: (11) 5572-4625</p>
         <p>Whatsapp: (11) 97814-9428</p>
       </div>
-
-      <a href={whatsappLink} target="_blank">
-        <img src={whatsappIcon} alt='Whatsapp Icon' />
-      </a>
 
       <p>&copy; 2024 Lina Hsu. Todos os direitos reservados.</p>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
 import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/LOGO_Keizen_Whatsapp.jpg";
+import './Header.css';
 
 function Header() {
   return (
     <>
-      <Link to="/home">
-        <img src={logo} alt="logo" width='100px' />
-      </Link>
+      <div className="header-container">
+        <Link to="/home" className="logo">
+          <img src={logo} alt="logo" width='100px' />
+        </Link>
 
-      <div>
-        <Link to="/about">Quem somos</Link>
-        <Link to="/staff">Nossa Equipe</Link>
-        <Link to="/services">Nossos Serviços</Link>
+        <div className="links-container">
+          <Link className="link" to="/about">Quem somos</Link>
+          <Link className="link" to="/staff">Nossa Equipe</Link>
+          <Link className="link" to="/services">Nossos Serviços</Link>
+        </div>
       </div>
 
       <Outlet />
