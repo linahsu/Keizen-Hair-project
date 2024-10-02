9a31d185b798eb379c48ca610bb108ffcd73a622
Lina Hsu <linahthsu@gmail.com>
2024-10-02 14:06:08 -0300

    feat: implements photos and roles to staff in the satff page

--- src/pages/Staff.tsx
+++ src/pages/Staff.tsx
@@ -1,11 +1,32 @@
+import keithPhoto from '../assets/staff/staff-keith.jpeg';
+import amandaPhoto from '../assets/staff/staff-amanda.jpeg';
+import edsonPhoto from '../assets/staff/staff-tar.jpeg';
+import aylaPhoto from '../assets/staff/staff-ayla.jpeg';
+import vitoriaPhoto from '../assets/staff/staff-vicky.jpeg';
+import alinePhoto from '../assets/staff/staff-aline.jpeg';
+import eduardoPhoto from '../assets/staff/staff-dudu.jpeg';
+import karinaPhoto from '../assets/staff/staff-karina.jpeg';
+
 function Staff() {
-  const staffNames: string[] = ['Keith Miamura', 'Amanda Matsune', 'Edson Gondo', 'Ayla Shizue', 'Vitória Secílio'];
-  const staffPhotos: {[key: string]: string} = {
-    'Keith Miamura': 'Keith é o fundador do nosso espaço e é especialista em cortes, colorações e tratamentos capilares.',
-    'Amanda Matsune': 'Amanda é especialista em penteados e maquiagem, com experiência em eventos e produções.',
-    'Edson Gondo': 'Edson é especialista em cortes e tratamentos capilares, com foco em cabelos crespos e cacheados.',
-    'Ayla Shizue': 'Ayla é especialista em colorações e tratamentos capilares, com foco em cabelos loiros e ruivos.',
-    'Vitória Secílio': 'Vitória é especialista em cortes e penteados, com foco em cabelos curtos e médios.'
+  const staffNames: string[] = [
+    'Keith Miamura',
+    'Amanda Matsune',
+    'Edson Gondo',
+    'Ayla Shizue',
+    'Vitória Secílio',
+    'Aline Sato',
+    'Eduardo Miamura',
+    'Karina Kael'];
+  
+  const staffInfo: {[key: string]: {[key: string]: string}} = {
+    'Keith Miamura': {photo: keithPhoto, role: 'Cabeleireiro'},
+    'Amanda Matsune': {photo: amandaPhoto, role: 'Cabeleireiro'},
+    'Edson Gondo': {photo: edsonPhoto, role: 'Cabeleireiro'},
+    'Ayla Shizue': {photo: aylaPhoto, role: 'Cabeleireiro'},
+    'Vitória Secílio': {photo: vitoriaPhoto, role: 'Cabeleireiro'},
+    'Aline Sato': {photo: alinePhoto, role: 'Auxiliar de Cabeleireiro'},
+    'Eduardo Miamura': {photo: eduardoPhoto, role: 'Auxiliar de Cabeleireiro'},
+    'Karina Kael': {photo: karinaPhoto, role: 'Recepcionista'},
   }
 
   return (
@@ -20,8 +41,10 @@ function Staff() {
       <div>
         {staffNames.map((name) => (
           <div key={name}>
-            <img src={staffPhotos[name]} alt={name} />
+            <img src={staffInfo[name]['photo']} alt={name} width="150"/>
             <h2>{name}</h2>
+            <p>{staffInfo[name]['role']}</p>
+            <br/>
           </div>
         ))}
       </div>
