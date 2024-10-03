6cff41aad46afa531426da1317c27e553831aad6
Lina Hsu <linahthsu@gmail.com>
2024-10-03 14:51:24 -0300

    feat: style home page with tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,43 +2,67 @@ import video from '../assets/video-corte-2.mp4';
 import mapa from '../assets/mapa.jpg';
 import Footer from '../components/Footer';
 import Carroulsel from '../components/Carroulsel';
-import './Home.css';
+// import './Home.css';
 
 function Home() {
   return (
     <>
-      <div className="home-container">
+      <div className="w-80/100 m-0 mx-auto">
       
-        <div className="first-container">
-          <div className="text-container">
-            <h1>Bem-vindo ao Studio Keizen!</h1>
-            <h2>
+        <div
+          className="
+            m-0 mx-auto mt-10 mb-176
+            rounded-3xl 
+            relative 
+            flex
+            "
+        >
+          <div
+            className="
+              ml-10 mt-10
+              absolute z-20
+              w-45/100
+              font-caveat
+              "
+            >
+            <h1 className="font-extrabold text-5xl text-aquamarine" >Bem-vindo ao Studio Keizen!</h1>
+            <h2 className="mt-3 text-3xl" >
               Cuidamos de você em um ambiente acolhedor, onde sua 
-              <span className="highlight"> #beleza natural</span> é realçada com 
+              <span className="text-4xl text-aquamarine font-bold"> #beleza natural</span> é realçada com 
               técnicas que facilitam o seu dia a dia.
             </h2>
           </div>
-          <video width="600" autoPlay loop muted>
+          <video
+            className="
+              w-full 
+              h-auto
+              absolute
+              rounded-xl 
+              opacity-40
+            "
+            width="600"
+            autoPlay loop muted
+          >
             <source src={video} type="video/mp4"/>
           </video>
         </div>
 
-        <div className='second-container'>
+        <div className="flex items-center">
           <Carroulsel />
-          <div className="map-container">
+          <div className="flex items-center">
             <a
-              className="map"
+              className="mx-5"
               href="https://www.google.com/maps/place/KEIZEN+by+KEITH+MIAMURA/@-23.6071083,-46.6369541,17z/data=!4m6!3m5!1s0x94ce5a4a3c24cddb:0x8fb38c98f97a70a7!8m2!3d-23.6064437!4d-46.63498!16s%2Fg%2F11c43mvfhr?entry=ttu&g_ep=EgoyMDI0MDkxOC4xIKXMDSoASAFQAw%3D%3D"
               target='_blank'
               rel='noreferrer noopener'
             >
-              <img src={mapa} alt='Mapa' />
+              <img className="rounded-lg w-128"  src={mapa} alt='Mapa' />
             </a>
-            <div className='map-text'>
-              <h2>Aonde estamos?</h2>
+            <div className='w-144'>
+              <h2 className="mb-2 text-lg font-bold text-[#ff6347] underline">Aonde estamos?</h2>
               <p>
                 Estamos na rua Afonso Celso, 1679, Vila Mariana. 
-                A 700m do <span className='metro-highlight'>metro Praça da Árvore</span>, 
+                A 700m do <span className='font-bold text-[#ff6347]'>metro Praça da Árvore</span>, 
                 leva cerca de 10 minutinhos a pé. Caso esteja de carro, a rua é tranquila 
                 e é fácil achar uma vaga.
               </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'caveat': ['Caveat', 'cursive'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
+        '256': '64rem',
+        '288': '72rem',
+        '45/100': '45%',
+        '80/100': '85%',
+      },
+      colors: {
+        'aquamarine': '#7fffd4',
+      }
+    },
   },
   plugins: [],
 }
